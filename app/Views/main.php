eebfb9505388550b7a2d359c80ccf61084985e89
Nycht1 <nasutionrachmat@gmail.com>
2024-05-06 15:45:47 +0700

    populate table rows with database records

--- app/Views/main.php
+++ app/Views/main.php
@@ -2,7 +2,7 @@
     <div class="col-md-12">
         <h3>Daftar Stok Barang di WarungKu</h3>
 
-        <a href=<?= base_url('addBarang') ?> class="btn btn-success mb-2">Tambah Barang</a>
+        <a href="<?= base_url('addBarang') ?>" class="btn btn-success mb-2">Tambah Barang</a>
 
         <table class="table table-bordered">
             <thead class="table-primary">
@@ -16,36 +16,25 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>MM</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td><button type="button" class="btn btn-primary">Edit</button>
-                        <button type="button" class="btn btn-danger">Hapus</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>MM</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td><button type="button" class="btn btn-primary">Edit</button>
-                        <button type="button" class="btn btn-danger">Hapus</button>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>MM</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td><button type="button" class="btn btn-primary">Edit</button>
-                        <button type="button" class="btn btn-danger">Hapus</button>
-                    </td>
-                </tr>
+                <?php if (!empty($products)) : ?>
+                    <?php foreach ($products as $index => $product) : ?>
+                        <tr>
+                            <th scope="row"><?= $index + 1 ?></th>
+                            <td><?= $product->id_barang ?></td>
+                            <td><?= $product->nama_barang ?></td>
+                            <td><?= $product->quantity ?></td>
+                            <td><?= $product->gambar_barang ?></td>
+                            <td>
+                                <a href="#" class="btn btn-primary">Edit</a>
+                                <a href="#" class="btn btn-danger">Hapus</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <tr>
+                        <td colspan="6" class="text-center">No data available</td>
+                    </tr>
+                <?php endif; ?>
             </tbody>
         </table>
 
