58d63905a133068d1fab7d1ff551655fd1946607
Rizky Bagaskara <rizky.6abu@hotmail.com>
2024-05-06 10:34:25 +0700

    feat: add routes and function to show addBarang.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -10,5 +10,17 @@ class Home extends BaseController
         echo view('main');
 
         echo view('footer');
+
+        echo view('about.php');
+
+    }
+
+    public function addBarang(){
+        // untuk sementara echo view dlu biar muncul
+        echo view('addBarang');
+    }
+
+    public function updateBarang(){
+        
     }
 }
