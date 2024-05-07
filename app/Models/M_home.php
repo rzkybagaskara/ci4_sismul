f0bb8e3d09c0592d9073d9de36035a503b9e07fa
Nycht1 <nasutionrachmat@gmail.com>
2024-05-07 12:22:37 +0700

    model functions

--- app/Models/M_home.php
+++ app/Models/M_home.php
@@ -11,12 +11,24 @@ class M_Home extends Model {
 
     protected $allowedFields = ['id_barang', 'nama_barang', 'quantity', 'gambar_barang'];
 
+    public function getProduct($id) {
+        return $this->find($id);
+    }
+
     public function getAllProducts() {
         return $this->findAll();
     }
 
+    public function deleteProduct($id) {
+        return $this->where('id_barang', $id)->delete();
+    }
+
     public function insertProduct($data) {
         return $this->insert($data);
+    }
+
+    public function updateProduct($id, $data) {
+        return $this->update($id, $data);
     }
 
 
