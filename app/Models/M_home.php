baee973f553de73b255cad2ead58fea8fcd54179
Nycht1 <nasutionrachmat@gmail.com>
2024-05-07 17:17:36 +0700

    fix updateBarang model syntax

--- app/Models/M_home.php
+++ app/Models/M_home.php
@@ -28,7 +28,7 @@ class M_Home extends Model {
     }
 
     public function updateProduct($id, $data) {
-        return $this->where('id_barang', $id)->update($data);
+        return $this->where('id_barang', $id)->set($data)->update();
     }
 
 
