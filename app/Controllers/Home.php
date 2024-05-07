34056e33f1c271032349237eeadf0accce2cee99
Nycht1 <nasutionrachmat@gmail.com>
2024-05-07 17:30:47 +0700

    updateBarang controller function

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -64,30 +64,48 @@ class Home extends BaseController {
             'quantity' => 'required'
         ]);
 
+        $data['products'] = $this->productModel->getProduct($id);
+
         if (!$validation->withRequest($this->request)->run()) {
-            return view('addBarang') . view('footer', ['validation' => $validation]);
+            return view('updateBarang', ['data' => $data['products']]) . view('footer', ['validation' => $validation]);
         } else {
-            $id = uniqid('item', TRUE);
-
-            $file = $this->request->getFile('gambar_barang');
-            $newName = $file->getRandomName();
-
             $config['upload_path'] = './upload/post';
             $config['allowed_types'] = 'jpeg|jpg|png';
             $config['max_size'] = 100000;
             $config['file_ext_tolower'] = true;
             $config['file_name'] = str_replace('.', '_', $id);
 
-            $file->move(ROOTPATH . 'upload/post', $newName);
+            $gambar_barang = $this->request->getFile('gambar_barang');
 
-            $data = [
-                'id_barang' => $id,
-                'nama_barang' => $this->request->getVar('nama_barang'),
-                'quantity' => $this->request->getVar('quantity'),
-                'gambar_barang' => $newName
-            ];
+            // Cek kalo ada gambar baru
+            if ($gambar_barang->isValid() && !$gambar_barang->hasMoved()) {
 
-            $this->productModel->updateProduct($data);
+                // Hapus gambar seblumyna kalo ada
+                $prev_image = $data['products']['gambar_barang'];
+                $newName = $gambar_barang->getRandomName();
+
+                if ($prev_image && file_exists('./upload/post/' . $prev_image)) {
+                    unlink('./upload/post/' . $prev_image);
+                }
+
+                // Simpen ke folder post
+                $gambar_barang->move(ROOTPATH . 'upload/post',  $newName);
+
+                // Update db pake nama baru
+                $data = [
+                    'nama_barang' => $this->request->getVar('nama_barang'),
+                    'quantity' => $this->request->getVar('quantity'),
+                    'gambar_barang' => $newName
+                ];
+            } else {
+                // Ga ada gambar baru, langsung ambil data dari input field
+                $data = [
+                    'nama_barang' => $this->request->getVar('nama_barang'),
+                    'quantity' => $this->request->getVar('quantity')
+                ];
+            }
+
+            $this->productModel->updateProduct($id, $data);
 
             return redirect()->to('/');
         }
